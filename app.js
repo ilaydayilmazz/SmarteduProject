3a2c1c086cc9fe6ed727e4202682fe9746e45907
ilaydayilmazz <63224483+ilaydayilmazz@users.noreply.github.com>
2023-11-01 12:48:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+
+app.get("/", (req, res)=>{
+    res.send("index sayfası");
+
+})
+
+const port = 3000;
+
+app.listen(port, ()=> {
+
+    console.log('app started on port ${port}')
+});
